b00071e52a83870ebf897e940bd6cf540f72b12b
Logan <41876727+lnguyen16@users.noreply.github.com>
2025-02-22 16:36:15 -0500

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,13 +3,7 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD45P1YhaCwZOjiddodOM0fbkAwSVY49vo",
-    authDomain: "heatmap-tracker.firebaseapp.com",
-    projectId: "heatmap-tracker",
-    storageBucket: "heatmap-tracker.firebasestorage.app",
-    messagingSenderId: "550298165891",
-    appId: "1:550298165891:web:b8a63d8db66e35fa8cf1b6",
-    measurementId: "G-5582XYYXRK"
+
 };
 
 // Initialize Firebase
